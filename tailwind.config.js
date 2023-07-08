06419088df8d2288f9db5fb0604a5ab819015327
mandriva19 <97590591+mandriva19@users.noreply.github.com>
2023-07-08 16:05:22 +0400

    final commit
    gfusbj43

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["'Red Hat Display', sans-serif;'"],
+      },
+
+      colors: {
+        impBlue: {
+          100: "hsl(224, 23%, 55%)",
+          200: "hsl(225, 100%, 98%)",
+          300: "hsl(225, 100%, 94%)",
+          400: "hsl(245, 75%, 52%)",
+          500: "hsl(223, 47%, 23%)",
+        },
+      },
+
+      backgroundImage: {
+        "mobile-pattern": "url('/images/pattern-background-mobile.svg')",
+        "desktop-pattern": "url('/images/pattern-background-desktop.svg')",
+      },
+    },
   },
   plugins: [],
 };
